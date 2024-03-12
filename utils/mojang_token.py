557329bb0c7f222bf37c65fc51b33ab756463a80
Miura738 <miura738@1oli.cn>
2024-03-12 20:06:51 +0800

    Update mojang_token.py

--- utils/mojang_token.py
+++ utils/mojang_token.py
@@ -1,4 +1,18 @@
 from mongo import db
+from json import dump
+from selenium import webdriver
+from urllib.parse import urlparse, parse_qs
+from requests import post, get
+
+
+def getCode(microsoft_url: str):
+    result = urlparse(microsoft_url)  # 会被重定向，获取一下 URL
+    code = None
+    if result.hostname == 'login.live.com' and result.path == '/oauth20_desktop.srf':
+        query = parse_qs(result.query)
+        if 'code' in query and 'lc' in query and 'error' not in query:
+            code = query['code'][0]
+    return code
 
 
 def get_mojang_token():
@@ -6,19 +20,12 @@ def get_mojang_token():
     if token is None:
         print("No mojang Token")
 
+    print("Please open this link in your browser:")
+    print("https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&response_type=code&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf")
+    code = getCode(input("Please enter redirect url: "))
+
 
 if __name__ == '__main__':
-    # get_mojang_token()
+    get_mojang_token()
 
-    import requests
 
-    code = "M.C510_BAY.2.U.434096df-05fc-035b-a766-35b459b509af"
-    url = f"https://login.live.com/oauth20_token.srf?client_id=00000000402b5328&code={code}&grant_type=authorization_code&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf"
-
-    headers = {
-          "Content-Type": "application/x-www-form-urlencoded"
-    }
-
-    response = requests.post(url, headers=headers)
-
-    print(response.text)
